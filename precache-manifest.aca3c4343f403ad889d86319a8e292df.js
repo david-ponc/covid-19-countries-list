297b59123df3bd0d989b5d66ee4209aa70bb0d9b
David Ponce Vargas <buap.david@gmail.com>
2020-05-25 17:40:02 -0500

    Updates

---
+++ precache-manifest.aca3c4343f403ad889d86319a8e292df.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05ad73764e81bc6e5421618859a69c3",
+    "url": "/react-covid-19/index.html"
+  },
+  {
+    "revision": "b13ac5544a11e73ee0e4",
+    "url": "/react-covid-19/static/css/main.f22aee13.chunk.css"
+  },
+  {
+    "revision": "68f34c4158e3cf4a6d4f",
+    "url": "/react-covid-19/static/js/2.9ff266ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-covid-19/static/js/2.9ff266ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13ac5544a11e73ee0e4",
+    "url": "/react-covid-19/static/js/main.899fb893.chunk.js"
+  },
+  {
+    "revision": "6168300571fa1c7951d2",
+    "url": "/react-covid-19/static/js/runtime-main.2fd007a3.js"
+  },
+  {
+    "revision": "58db829c672552f5ded58faaa3b0eee5",
+    "url": "/react-covid-19/static/media/icon-dead.58db829c.svg"
+  },
+  {
+    "revision": "a06447c6f9de3bf7e16ab203d8b6a8a1",
+    "url": "/react-covid-19/static/media/icon-filter.a06447c6.svg"
+  },
+  {
+    "revision": "5ede684757d0773a5a966576aee1bfc5",
+    "url": "/react-covid-19/static/media/icon-mode.5ede6847.svg"
+  },
+  {
+    "revision": "ecc7d48eed5f9579441f6e1dc2fdc349",
+    "url": "/react-covid-19/static/media/icon-recovered.ecc7d48e.svg"
+  },
+  {
+    "revision": "42b9c8d0c9d6056285b64ec74f630dbb",
+    "url": "/react-covid-19/static/media/icon-search.42b9c8d0.svg"
+  },
+  {
+    "revision": "931b8e8c6d1b70de0dbbf32a0819a002",
+    "url": "/react-covid-19/static/media/icon-up.931b8e8c.svg"
+  }
+]);
